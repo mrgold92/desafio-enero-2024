03379d1c0c2785fe5876624dd9fd7475e02e0008
David Salazar <davids1_4@hotmail.com>
2024-01-20 12:31:30 +0100

    Fix word inversion logic in invertirPalabras method

--- src/main/java/com/programandoenjava/parte1/Parte1.java
+++ src/main/java/com/programandoenjava/parte1/Parte1.java
@@ -4,12 +4,32 @@ public class Parte1 {
 
     /**
      * Escribe un programa en Java que reciba una cadena de texto y devuelva la
-     * misma cadena pero con cada palabra invertida. Por ejemplo, para la entrada "Hola Mundo",
+     * misma cadena pero con cada palabra invertida. Por ejemplo, para la entrada
+     * "Hola Mundo",
      * la salida debería ser "aloH odluM".
+     * 
      * @param cadena cadena de entrada
      * @return cadena invertida
      */
-    public String invertirPalabras(final String cadena) {;
-        return null;
+    public String invertirPalabras(final String cadena) {
+        if (cadena == null) {
+            return "";
+        }
+    
+        String resultado = "";
+        String palabra = "";
+    
+        for (int i = 0; i < cadena.length(); i++) {
+            char c = cadena.charAt(i);
+    
+            if (Character.isLetterOrDigit(c)) {
+                palabra = c + palabra;
+            } else {
+                resultado += palabra + c;
+                palabra = "";
+            }
+        }
+    
+        return resultado + palabra;
     }
 }
